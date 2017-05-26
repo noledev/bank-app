e35913c4a36d33762b1988557a4362fd61c0b26d
noledev <eric.mitchell83@gmail.com>
2017-05-25 21:53:33 -0400

    Minor update

---
+++ api/api.ts
@@ -0,0 +1,9 @@
+export * from './AccountApi';
+import { AccountApi } from './AccountApi';
+export * from './AccountsApi';
+import { AccountsApi } from './AccountsApi';
+export * from './TransferApi';
+import { TransferApi } from './TransferApi';
+export * from './ValuesApi';
+import { ValuesApi } from './ValuesApi';
+export const APIS = [AccountApi, AccountsApi, TransferApi, ValuesApi];
